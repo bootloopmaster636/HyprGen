f9bc7bc03f547ba9fcf7cb52b01f80eff25bc2dd
bootloopmaster636 <christhartono@hotmail.com>
2023-11-22 13:33:26 +0700

    💄 now settings tile will only expand when hovered, to save space

--- src/Components/SettingsTile.tsx
+++ src/Components/SettingsTile.tsx
@@ -4,9 +4,17 @@ import { ThemeData } from "../consts.ts";
 export default ({ title, description }) => {
   return (
     <div
-      className={ThemeData.backgroundDeep + " h-[12rem] w-[20rem] rounded-xl"}
+      className={
+        ThemeData.backgroundDeep +
+        " h-[8rem] min-w-[18rem] max-w-[24rem] rounded-xl transition-all hover:h-[12rem]"
+      }
     >
-      <div className={ThemeData.surface + " h-[9rem] w-[20rem] rounded-xl p-4"}>
+      <div
+        className={
+          ThemeData.surface +
+          " h-[8rem] min-w-[18rem] max-w-[24rem] rounded-xl p-4"
+        }
+      >
         <p className={ThemeData.onPrimary + " text-bold text-xl"}>{title}</p>
         <div className={"h-20 overflow-y-scroll"}>
           <p className={"text-md"}>{description}</p>
